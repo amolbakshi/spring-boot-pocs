8403725d9d1a2f984f2e98fd512f59edae7ca878
Amol <amolbakshi06@gmail.com>
2021-06-21 01:13:53 -0400

    1st commit

---
+++ spring-cloud-data-flow/data-flow/src/main/java/com/bakshi/poc/dataflow/DataFlowApplication.java
@@ -0,0 +1,13 @@
+package com.bakshi.poc.dataflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataFlowApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-data-flow/data-flow/src/test/java/com/bakshi/poc/dataflow/DataFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakshi.poc.dataflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
